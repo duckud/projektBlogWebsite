dfa8d47792e1898b0364c6be890ea67e16318e6e
Elisa <E.Petit.inf21@stud.bbbaden.ch>
2022-12-21 11:36:53 +0100

    .

--- Website/JS/app.js
+++ Website/JS/app.js
@@ -1,20 +1,34 @@
-const form = document.getElementById('myform')
-form.addEventListener('submit', function (ev) {
-  ev.preventDefault()
-  try {
-    const Benutzername = document.getElementById('Benutzername')
-    const passwort = document.getElementById('Passwort')
+// const form = document.getElementById('myform')
+// form.addEventListener('submit', function (ev) {
+//   ev.preventDefault()
+//   try {
+//     const Benutzername = document.getElementById('Username')
+//     const passwort = document.getElementById('password')
 
-    if (Benutzername.value.length < 5) {
-      throw new Error('Benutzername ist zu kurz')
-    }
-    if (Benutzername.value.length > 20) {
-      throw new Error('Benutzername ist zu lang')
-    }
-    if (passwort.value.length < 12) {
-      throw new Error('passwort ist zu kurz')
-    }
-  } catch (error) {
-    alert('Funktioniert nicht: ${error.message}')
+//     if (Benutzername.value.length < 5) {
+//       throw new Error('Username is to short')
+//     }
+//     if (Benutzername.value.length > 20) {
+//       throw new Error('Username is to long')
+//     }
+//     if (passwort.value.length < 12) {
+//       throw new Error('password is to short')
+//     }
+//   } catch (error) {
+//     alert('Funktioniert nicht: ${error.message}')
+//   }
+// })
+function checkform (ev) {
+  const Benutzername = document.getElementById('Username')
+  const passwort = document.getElementById('password')
+
+  if (Benutzername.value.length < 5) {
+    throw new Error('Username is to short')
   }
-})
+  if (Benutzername.value.length > 20) {
+    throw new Error('Username is to long')
+  }
+  if (passwort.value.length < 8) {
+    throw new Error('Password is to short')
+  }
+}
